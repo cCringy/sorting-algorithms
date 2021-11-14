034b42a8e420bc276d817ecc283ab07c9426f895
Cringy <rofi2402@gmail.com>
2021-11-14 15:29:22 +0100

    based by clement

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SortingVisualizer from './components/SortingVisualizer.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SortingVisualizer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./SortingVisualizer.css"
+
+
+export default class SortingVisualizer extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            array: []
+        };
+    }
+    componentDidMount(){
+        this.resetArray();
+    }
+
+    resetArray(){
+        const barArray = [];
+        for(let i = 0; i<100; i++){
+            barArray.push(randomIntFromInterval(5,1000));
+        }
+        this.setState({barArray});
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <>
+              {array.map((value,index) => (
+                  <div className = "array-bar" key={index}>
+                    {value}
+                  </div>
+              ))}
+            </>
+        )
+    }
+    
+}
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
